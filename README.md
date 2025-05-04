bf3735ef5f695bb0639344b5b257b885b71c7d5d
Dylan Swanson <daswanson22@gmail.com>
2025-05-04 15:11:21 -0700

    First commit, basic homepage

---
+++ README.md
@@ -0,0 +1,25 @@
+# Scope #
+This project is intended to help people in the San Diego area find fun events going on.
+
+# Database # 
+
+Users will be able to filter for events based on tags, categories, and location.
+
+## Tables ##
+- User
+- City
+- Event
+- Category
+- Tag
+
+# User Interface #
+This section is intended to be guidlines to the styling of the website.
+
+## Key Characteristics ##
+Sleek, Modern, Easy-to-Use
+
+## Colors ##
+- Black #000000
+- White #FFFFFF
+- Light Blue #28d2f4
+- Pink #f428c1
---
+++ events/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'events'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # path('events/', views.event_list, name='event_list'),
+    # path('events/<int:event_id>/', views.event_detail, name='event_detail'),
+    # path('events/create/', views.event_create, name='event_create'),
+    # path('events/<int:event_id>/update/', views.event_update, name='event_update'),
+    # path('events/<int:event_id>/delete/', views.event_delete, name='event_delete'),
+]
